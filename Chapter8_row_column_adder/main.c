ee489a51c6560295abc304b04500ec14150dc056
Nicholas Morris <PrinceMorris@nicholas-morriss-imac.local>
2018-12-14 10:40:15 +1000

    Initial Commit

---
+++ Chapter8_row_column_adder/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chapter8_row_column_adder
+//
+//  Created by Nicholas Morris on 14/12/18.
+//  Copyright © 2018 Nicholas Morris. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
